d6774f4b185d6a90a525216053990bb022d66651
ThibaudP <t.poncin@gmail.com>
2020-12-08 11:08:16 +0000

    Fix folder name

---
+++ 0x01-python-if_else_loops_function/13-insert_number.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts node in a sorted linked list
+ *
+ * @head: head node to list
+ * @number: number to add in new node
+ *
+ * Return: address to new node
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *cur = *head;
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	if (*head == NULL)
+		*head = new;
+	
+	if (number < cur->n)
+	{
+		new->next = cur;
+		new->n = number;
+		*head = new;
+	}
+	else
+	{
+		while (cur->next && number > cur->next->n)
+			cur = cur->next;
+
+		new->n = number;
+		new->next = cur->next;
+		cur->next = new;
+	}
+
+	return (new);
+
+}
