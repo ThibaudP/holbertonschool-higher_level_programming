c5c218349100b11dc46597f94885968996769496
ThibaudP <t.poncin@gmail.com>
2020-12-08 11:09:50 +0000

    Fix folders

--- 0x01-python_if_else_loops_functions/101-remove_char_at.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def remove_char_at(str, n):
-    new = str[:n] + str[n+1:] 
-    return new
--- 0x01-python_if_else_loops_functions/7-islower.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def islower(c):
-    asc = ord(c)
-    if asc >= 97 and asc <= 122:
-        return True
-    else:
-        return False
