5ede10db16e8bcd110d0e00685cec3f2bffed30a
ThibaudP <t.poncin@gmail.com>
2020-12-15 15:16:09 +0000

    Fix 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if 0 <= idx < len(my_list):
+    if my_list and 0 <= idx < len(my_list):
         return (my_list[idx])
-    return (my_list)
+    else:
+        return (None)
