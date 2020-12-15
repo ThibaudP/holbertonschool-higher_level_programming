9308cdd771de6fb213c4aa44476b633d378ea0f9
ThibaudP <t.poncin@gmail.com>
2020-12-15 15:37:32 +0000

    Add 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    if my_string:
+        for x in my_string:
+            if x not in "cC":
+                new_string += x
+    return new_string
