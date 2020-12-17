1c8b31402a575b5f1de2bc3818fa37d6525dfd96
ThibaudP <t.poncin@gmail.com>
2020-12-17 20:11:01 +0000

    Fix 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,3 +4,5 @@ def weight_average(my_list=[]):
         # return sum(a * b for a, b in my_list) / sum(b for a, b in my_list)
         dico = dict(my_list)
         return sum(k * v for k, v in dico.items()) / sum(dico.values())
+    else:
+        return 0
