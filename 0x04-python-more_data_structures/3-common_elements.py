4ba58971fd85937b49997dbffc9e79370f309f5d
ThibaudP <t.poncin@gmail.com>
2020-12-17 11:14:37 +0000

    Fix 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,4 +2,9 @@
 def common_elements(set_1, set_2):
     if set_1 and set_2:
         return {x for x in set_1 if x in set_2}
-    return None
+    elif set_1 and not set_2:
+        return set_1
+    elif set_2 and not set_1:
+        return set_2
+    else:
+        return None
