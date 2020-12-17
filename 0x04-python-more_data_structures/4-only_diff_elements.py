709235ac369c2d65d50bc7c61f85836b304367d9
ThibaudP <t.poncin@gmail.com>
2020-12-17 14:25:32 +0000

    Refactor 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     if set_1 and set_2:
-        return set_1.symmetric_difference(set_2)
-    elif set_1 and not set_2:
-        return set_1
-    elif set_2 and not set_1:
-        return set_2
-    else:
-        return set()
+        return [x for x in set_1 if x not in set_2] + [x for x in set_2 if x not in set_1]
