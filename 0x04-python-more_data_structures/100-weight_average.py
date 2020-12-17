438db5025897467ea0368c996ed24ade99e42cb7
ThibaudP <t.poncin@gmail.com>
2020-12-17 20:09:58 +0000

    Add 100 & 101

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list:
+        # return sum(a * b for a, b in my_list) / sum(b for a, b in my_list)
+        dico = dict(my_list)
+        return sum(k * v for k, v in dico.items()) / sum(dico.values())
