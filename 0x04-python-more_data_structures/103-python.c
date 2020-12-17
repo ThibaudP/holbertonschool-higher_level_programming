c9af2e7e1431fd370db509bf6cbde2d13f195c5f
ThibaudP <t.poncin@gmail.com>
2020-12-17 20:21:32 +0000

    Add placeholder for 103

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list - prints a Python list
+ *
+ * @p: a python object
+ */
+
+void print_python_list(PyObject *p)
+{
+	(void)p;
+}
+
+/**
+ * print_python_bytes - prints Python bytes
+ *
+ * @p: a python object
+ */
+
+void print_python_bytes(PyObject *p)
+{
+	(void)p;
+}
