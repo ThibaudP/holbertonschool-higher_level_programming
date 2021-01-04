fa1fa5156e39c7296d2efa6e10c1e88d831815d3
ThibaudP <t.poncin@gmail.com>
2021-01-04 11:23:24 +0000

    Fix 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,4 +13,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             res_list.append(0)
             print("out of range")
-    return res_list
+        finally:
+            return res_list
