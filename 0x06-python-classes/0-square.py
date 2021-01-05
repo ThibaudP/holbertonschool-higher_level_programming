615e36c5919bb7516bf03aa553ccc8f414bb4361
ThibaudP <t.poncin@gmail.com>
2021-01-05 14:50:07 +0000

    Fix doc

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 class Square:
-    """This class contains a square"""
+    """This class contains a square
+
+    Attributes:
+        __size (int): size of the square
+        __position (tuple): position of top left corner
+
+    """
     pass
