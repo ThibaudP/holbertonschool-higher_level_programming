f1f4545d2b298cb4b8df3238145e0438a9f0b951
ThibaudP <t.poncin@gmail.com>
2021-01-05 14:52:19 +0000

    Fix doc

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 class Square:
-    """This class contains a square"""
+    """This class contains a square
+    
+    The square shall be of number 1, as 1 shall be the number of squares
+    
+    """
     pass
