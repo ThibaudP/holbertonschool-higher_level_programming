f3519d10f91ba68c978a7521ca046c885275e6e5
ThibaudP <t.poncin@gmail.com>
2021-01-05 14:57:38 +0000

    Fix doc

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""Square module.
+
+This module contains a class that defines a square, its size,
+as well as methods to draw it with and without XY position offset.
+
+"""
+
+
 class Square:
     """This class contains a square
 
