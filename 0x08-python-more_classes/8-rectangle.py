a0e2fb4d26cd6d567a1c69c3d9d5da3536670d87
ThibaudP <t.poncin@gmail.com>
2021-01-11 18:05:00 +0000

    Fix exceptions

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -31,9 +31,9 @@ class Rectangle:
             (Rectangle): the biggest rectangle
         """
         if type(rect_1) is not Rectangle:
-            raise TypeError("rect_1 must be a Rectangle")
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if type(rect_2) is not Rectangle:
-            raise TypeError("rect_2 must be a Rectangle")
+            raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_2.area() > rect_1.area():
             return rect_2
         else:
