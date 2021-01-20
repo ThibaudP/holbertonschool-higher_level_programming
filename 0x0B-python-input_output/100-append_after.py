478ed7f38e8e9753b6b5378420af9885f65684ea
ThibaudP <t.poncin@gmail.com>
2021-01-20 18:15:56 +0000

    Add 100

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""append_after module"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Reads a file, insert new_line after search_string if it exists"""
+    new = ""
+    with open(filename, "r") as file:
+        for line in file:
+            new += line
+            if search_string in line:
+                new += new_string
+        
+    with open(filename, "w") as file:
+        file.write(new)    
