cdc50625d7f04d96968e020e808c9b59fcf793d6
ThibaudP <t.poncin@gmail.com>
2021-01-20 17:41:19 +0000

    Fix 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Prints the content of a file"""
     if type(filename) is str:
         with open(filename, "r", encoding="utf-8") as file:
-            print(file.read())
+            print(file.read(), end='')
