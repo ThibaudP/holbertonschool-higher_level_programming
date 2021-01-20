b04abc73f5faaead11b00de89cc2ad812897c6ed
ThibaudP <t.poncin@gmail.com>
2021-01-20 18:42:39 +0000

    Add 101

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""101-stats module"""
+import sys
+import signal
+
+def print_stats(file_size, status_codes):
+    """prints the stats"""
+    print("File size: {:d}".format(file_size))
+    for k, v in sorted(status_codes.items()):
+        if v != 0:
+            print("{:s}: {:d}".format(k, v))
+
+def sigint_handler(sig, frame):
+    """calls print_stats when SIGINT is captured"""
+    print_stats(file_size, status_codes)
+
+file_size = 0
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+                '403': 0, '404': 0, '405': 0, '500': 0}
+cnt = 0
+
+for line in sys.stdin:
+    signal.signal(signal.SIGINT, sigint_handler)
+    if cnt > 0 and cnt % 10 == 0:
+        print_stats(file_size, status_codes)
+    
+    words = line.split()
+    file_size += int(words[8])
+    status_codes[words[7]] += 1
+    cnt += 1
