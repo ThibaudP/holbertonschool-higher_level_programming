f8a73e9b116969e7e6090595293b2ca34f6d99d9
ThibaudP <t.poncin@gmail.com>
2021-01-20 18:45:18 +0000

    101 test

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python3
-"""101-stats module"""
+"""
+reads stdin line by line and computes metrics
+"""
 import sys
-import signal
-
-def print_stats(file_size, status_codes):
-    """prints the stats"""
-    print("File size: {:d}".format(file_size))
-    for k, v in sorted(status_codes.items()):
-        if v != 0:
-            print("{:s}: {:d}".format(k, v))
-
-def sigint_handler(sig, frame):
-    """calls print_stats when SIGINT is captured"""
-    print_stats(file_size, status_codes)
 
 file_size = 0
-status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-                '403': 0, '404': 0, '405': 0, '500': 0}
-cnt = 0
+status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+i = 0
+try:
+    for line in sys.stdin:
+        tokens = line.split()
+        if len(tokens) >= 2:
+            a = i
+            if tokens[-2] in status_tally:
+                status_tally[tokens[-2]] += 1
+                i += 1
+            try:
+                file_size += int(tokens[-1])
+                if a == i:
+                    i += 1
+            except:
+                if a == i:
+                    continue
+        if i % 10 == 0:
+            print("File size: {:d}".format(file_size))
+            for key, value in sorted(status_tally.items()):
+                if value:
+                    print("{:s}: {:d}".format(key, value))
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
 
-for line in sys.stdin:
-    signal.signal(signal.SIGINT, sigint_handler)
-    if cnt > 0 and cnt % 10 == 0:
-        print_stats(file_size, status_codes)
-    
-    words = line.split()
-    file_size += int(words[8])
-    status_codes[words[7]] += 1
-    cnt += 1
+except KeyboardInterrupt:
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
