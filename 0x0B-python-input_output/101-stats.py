762c4b2b577dbe667eec2c0859ec51d3b66d31d6
ThibaudP <t.poncin@gmail.com>
2021-01-20 19:53:05 +0000

    Fix 101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -11,17 +11,25 @@ def print_stats(file_size, status_codes):
             print("{:s}: {:d}".format(k, v))
 
 file_size = 0
-status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-                '403': 0, '404': 0, '405': 0, '500': 0}
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
 cnt = 0
 
 try:
     for line in sys.stdin:
-        words = line.split()
-
-        file_size += int(words[-2])
-        status_codes[words[-1]] += 1
-        cnt += 1
+        words = line.split(' ')
+        if len(words) == 9:
+            chk = cnt
+            if words[-2] in status_codes:
+                status_codes[words[-2]] += 1
+                cnt += 1
+            try:
+                file_size += int(words[-1])
+                if chk == cnt:
+                    cnt += 1
+            except:
+                if chk == cnt:
+                    continue
         if cnt % 10 == 0:
             print_stats(file_size, status_codes)
 
