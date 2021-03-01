d80b7c26fc3a12811949eb534d27bbde333f32da
ThibaudP <t.poncin@gmail.com>
2021-03-01 16:05:18 +0000

    fix(): missing semicolons

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- display the max temp in every state
 
-SELECT `state`, MAX(`value`) AS `max_temp` FROM `temperatures` GROUP BY `state` ORDER BY `state`
+SELECT `state`, MAX(`value`) AS `max_temp` FROM `temperatures` GROUP BY `state` ORDER BY `state`;
