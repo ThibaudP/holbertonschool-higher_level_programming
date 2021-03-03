58c2bf8ef5a81948ed2120fca81212f8d32436ed
ThibaudP <t.poncin@gmail.com>
2021-03-03 00:27:28 +0000

    fix(): typos

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -3,7 +3,7 @@
 SELECT `tv_shows`.`title`
 FROM `tv_shows`
 WHERE `tv_shows`.`id` NOT IN (
-    SELECT `tv_shows`.`title`
+    SELECT `tv_shows`.`id`
     FROM `tv_shows`
     JOIN `tv_show_genres`
     ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
