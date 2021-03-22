69b6d03a52babf2c9cd348a7d9bf4e594de829ba
ThibaudP <t.poncin@gmail.com>
2021-03-22 14:48:44 +0000

    fix(): fix 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,6 +5,7 @@ if (process.argv.length < 4) {
   console.log('0');
 } else {
   const array = process.argv.slice(2);
+  array.map(x => parseInt(x));
   array.sort();
   console.log(array[array.length - 2]);
 }
