40c761f7b3c6ea9f403c5bf270e2f817b38aaf44
ThibaudP <t.poncin@gmail.com>
2021-03-22 11:39:46 +0000

    fix(): wrong condition

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Prints the first argument passed to the script
 
-if (process.argv.length > 2) {
-  console.log(process.argv[2]);
-} else {
+if (process.argv[2] === undefined) {
   console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
