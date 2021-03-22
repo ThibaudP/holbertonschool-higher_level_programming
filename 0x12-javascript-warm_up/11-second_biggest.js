09f8b6979e75bc46ffa611b2311d6055c2a02dd5
ThibaudP <t.poncin@gmail.com>
2021-03-22 15:13:54 +0000

    fix(): add comparator function in array.sort()

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,6 +6,6 @@ if (process.argv.length < 4) {
 } else {
   let array = process.argv.slice(2);
   array = array.map(x => Number(x));
-  array.sort();
+  array.sort((a, b) => a - b);
   console.log(array[array.length - 2]);
 }
