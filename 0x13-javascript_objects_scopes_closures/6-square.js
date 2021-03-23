8178d379ffbe8041882f966d8e86bbe859625f31
ThibaudP <t.poncin@gmail.com>
2021-03-23 16:19:15 +0000

    fix(): remove unneeded constructor

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,10 +3,6 @@
 const OldSquare = require('./4-rectangle.js');
 
 module.exports = class Square extends OldSquare {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
