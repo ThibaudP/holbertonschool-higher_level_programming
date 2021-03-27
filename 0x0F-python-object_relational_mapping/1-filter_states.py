700f13468895511509fb76feee4170d8bd5f144a
ThibaudP <t.poncin@gmail.com>
2021-03-27 19:21:28 +0000

    fix(): remove var for cursor.execute()

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,9 +10,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
 
-    sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
-
-    cursor.execute(sql)
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
     rows = cursor.fetchall()
 
     for row in rows:
